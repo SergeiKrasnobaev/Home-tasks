6b1933b867c7c7e6a141d849a80b00803a229a31
Sergei Krasnobaev <geekbrainsme@gmail.com>
2022-11-13 15:42:18 +0300

    commit Task27

---
+++ Task25/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+//  и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число A : ");
+int digitA = Convert.ToInt32(Console.ReadLine());
+if (digitA != 0)
+{
+    Console.WriteLine("Введите число B : ");
+    int digitB = Convert.ToInt32(Console.ReadLine());
+    if (digitB > 0 && digitB % 1 == 0)
+    {
+        int stepen = Stepen(digitA, digitB);
+        Console.WriteLine($"Ответ: A в степени В -> {stepen}");
+    }
+    else
+    {
+        Console.WriteLine("Некорректный ввод. Число B натуральное.");
+    }
+}
+else
+{
+    Console.WriteLine("Некорректный ввод. Число A не равно 0");
+}
+
+int Stepen(int numA, int numB)
+{
+    int result = 1;
+    for (int i = 1; i <= numB; i++)
+    {
+        result = result * numA;
+    }
+    return result;
+}
+
+
+
+
+
+
---
+++ Task27/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число >= 10");
+int i = Convert.ToInt32(Console.ReadLine());
+if (i != 0 && i >= 10)
+{
+    int Sum(int n)
+    {
+        int sum = 0;
+        while (n > 0)
+        {
+            int num = n % 10;
+            n = n / 10;
+            sum = sum + num;
+        }
+        return sum;
+    }
+    Console.WriteLine($"Сумма цифр в числе {i} равна: {Sum(i)}");
+}
+else
+{
+    Console.WriteLine("Некорректный ввод");
+}
+
