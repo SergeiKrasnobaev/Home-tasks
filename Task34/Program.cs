3f027ad00392c20e50b97eed6937630f91391691
Sergei Krasnobaev <geekbrainsme@gmail.com>
2022-11-16 01:03:38 +0300

    commit Task34

---
+++ Task34/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+Console.WriteLine("Укажите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] RandomArray(int num)
+{
+    int[] array = new int[num];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(100, 999); //трёхзначные числа
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]} , ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+int EvenNumArray(int[] array)
+{
+    int evenCount = 0; // счётчик чётных чисел
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            evenCount = evenCount + 1;
+        }
+    }
+    return evenCount;
+}
+
+int[] r = RandomArray(n);
+PrintArray(r);
+int e = EvenNumArray(r);
+Console.WriteLine();
+Console.WriteLine($"Количество чётных чисел: {e} ");
+
+
+
+
+
+
+
+
+
+
